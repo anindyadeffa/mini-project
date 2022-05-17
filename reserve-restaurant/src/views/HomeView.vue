069c534b2ac6a87c40d7db8b510df0f7ab85e431
anindyadeffanabila@gmail.com <66814960+anindyadeffa@users.noreply.github.com>
2022-05-17 16:20:19 +0700

    add homepage

---
+++ reserve-restaurant/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <banner-home/>
+      <div class="container">
+        <package-option/>
+      </div>
+      <footer-home/>
+  </div>
+</template>
+
+<script>
+import BannerHome from '@/components/BannerHome.vue'
+import PackageOption from '@/components/PackageOption.vue'
+import FooterHome from '@/components/FooterHome.vue'
+
+  export default {
+    name: 'HomeView',
+
+    components: {
+      BannerHome,
+      PackageOption,
+      FooterHome,
+    },
+  }
+</script>
