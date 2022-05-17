62b040ca28b84db9ec433550af0564d378e7bf48
anindyadeffanabila@gmail.com <66814960+anindyadeffa@users.noreply.github.com>
2022-05-17 16:14:25 +0700

    add main components

---
+++ reserve-restaurant/src/components/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-navbar toggleable="md" variant="light" class="p-0 m-0">
+    <b-container>
+
+    <b-navbar-brand href="/">
+      <b-img :src="require('@/assets/img/logo/icon-logo.svg')" alt="Icon Logo" width="38px" class="m-0"></b-img>
+    </b-navbar-brand>
+
+    <!-- <b-navbar-toggle target="nav-collapse"></b-navbar-toggle> -->
+
+    <!-- <b-collapse id="nav-collapse" is-nav> -->
+        <!-- Right aligned nav items -->
+        <!-- <b-navbar-nav class="ml-auto">
+          <b-nav-item href="/">Home</b-nav-item>
+          <b-nav-item href="#">Package</b-nav-item>
+          <b-nav-item href="#">News</b-nav-item>
+          <b-nav-item href="#">Contact</b-nav-item>
+        </b-navbar-nav> -->
+      <!-- </b-collapse> -->
+    </b-container>
+  </b-navbar>
+</template>
+
+<script>
+  export default {
+    name: 'NavBar',
+  }
+</script>
+
+<style scoped>
+.nav-item {
+  margin: 0 15px 0 15px;
+}
+</style>
